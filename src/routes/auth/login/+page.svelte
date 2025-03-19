3ae5fa6c544e551e7006ed965710b90aae417268
Unrealrojo234 <otienotyan812@gmail.com>
2025-03-19 21:26:31 +0300

    Yeah

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -105,7 +105,6 @@
 		<br />
 
 		<button class="submit btn btn-success">Login</button>
-		<p class="signin">Don't have an account? <a href="/auth/signup">Signup</a></p>
 		<p class="signin"><a href="/auth/forgotPassword/">Forgot Password</a></p>
 	</form>
 </main>
